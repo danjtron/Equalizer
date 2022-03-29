a059c43ea716208a23694222fae89dd2245b4178
Daniel Choi <danielchoi@macbook-pro.lan>
2022-03-28 22:10:04 -0400

    changed a lot...

--- led_equalizer.cpp
+++ led_equalizer.cpp
@@ -17,23 +17,25 @@
 #define ADC_VREF 3.3
 #define ADC_RANGE (1 << 12)
 #define ADC_CONVERT (ADC_VREF / (ADC_RANGE - 1))
+//#define STDIO 1
 
-#define SPEED_BIN 9
+#define FREQ_BIN 9
 #define ADC_CLOCK_DIV 2500
 #define FREQ_BIN_STEP 48000000.0/ADC_CLOCK_DIV/CAPTURE_DEPTH
 
-#define MIN_MAGNITUDE_RANGE 0.04 //This is the lowest we will drop the fullscale range for outputPower
+#define MIN_MAGNITUDE_RANGE 0.065 //This is the lowest we will drop the fullscale range for outputPower
 #define MAG_RANGE_ADJUST_SPEED 1 // This is a value between 0 and 1 to adjust how fast the display adjusts to volume changes
 #define WS2812B_LED_PIN 2
 #define LED_LENGTH 135
 #define LED_ROWS 15
+#define LED_ZIG_ZAG true
 #define MAX_LED_BRIGHTNESS 255
 #define MIN_LED_BRIGHTNESS 50
 #define BRIGHTNESS_FADE 30
+#define NOISE_LEVEL 0.005 // Anything below this level, treat it as absolutely quiet
 
 #define DISPLAY_ROWS 15
 #define DISPLAY_COL 9
-
 
 // Vector Magnitude Approximation Variables https://en.wikipedia.org/wiki/Alpha_max_plus_beta_min_algorithm
 #define ALPHA 0.9604
@@ -54,7 +56,9 @@ void sample(uint16_t *capture);
 
 void setup()
 {
-    stdio_init_all();
+    #ifdef STDIO
+        stdio_init_all();
+    #endif
 
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
@@ -81,7 +85,7 @@ void setup()
     adc_set_clkdiv(ADC_CLOCK_DIV);
 
     printf("Arming DMA\n");
-    sleep_ms(1000);
+    sleep_ms(500);
     // Set up the DMA to start transferring data as soon as it appears in FIFO
     dma_chan = dma_claim_unused_channel(true);
     cfg = dma_channel_get_default_config(dma_chan);
@@ -93,6 +97,7 @@ void setup()
 
     // Pace transfers based on availability of ADC samples
     channel_config_set_dreq(&cfg, DREQ_ADC);
+    sleep_ms(500);
 }
 
 void sample(uint16_t *capture)
@@ -106,7 +111,6 @@ void sample(uint16_t *capture)
 
     adc_run(true);
 
-
     // Once DMA finishes, stop any new conversions from starting, and clean up
     // the FIFO in case the ADC was still mid-conversion.
     dma_channel_wait_for_finish_blocking(dma_chan);
@@ -117,25 +121,31 @@ void sample(uint16_t *capture)
 
 void displayUpdate(uint8_t *Intensity) {
   int color_hsv = 0;
-  
+  int led_index = 0;
   PicoLed::Color leds[LED_LENGTH];
+  
   for (int i = 0; i < DISPLAY_COL; i++) {
     for (int j = 1; j <= DISPLAY_ROWS; j++) {
-      if (j <= Intensity[i]) {                              // Light everything within the intensity range
-          bright[i*DISPLAY_ROWS + j-1] = MAX_LED_BRIGHTNESS; //((bright[i*DISPLAY_ROWS + j-1] + FADE) <= BRIGHTNESS) ? bright[i*DISPLAY_ROWS + j-1] + FADE : BRIGHTNESS;
-          ledStrip.setPixelColor(i*DISPLAY_ROWS + j-1,PicoLed::HSV(color_hsv,255,MAX_LED_BRIGHTNESS));
-          //This line is left here for reference
-          //leds[i*DISPLAY_ROWS + j-1] = PicoLed::HSV(color_hsv,255,MAX_LED_BRIGHTNESS);
+        led_index = (i*DISPLAY_ROWS -1) + j;
+        // Every second column flip it due to wiring of LEDs
+        if (LED_ZIG_ZAG == true){
+            if (i%2 != 0)
+                led_index = (i*DISPLAY_ROWS -1) + 16 - j;
+        }
+      
+        // Only light display if there are more than 4 LEDs to light up. This should kill any random noise
+        if (j <= Intensity[i] ) { 
+            bright[led_index] = MAX_LED_BRIGHTNESS; //((bright[i*DISPLAY_ROWS + j-1] + FADE) <= BRIGHTNESS) ? bright[i*DISPLAY_ROWS + j-1] + FADE : BRIGHTNESS;
+            ledStrip.setPixelColor(led_index,PicoLed::HSV(color_hsv,255,MAX_LED_BRIGHTNESS));
 
-      }
-      else {                                                 // Everything outside the range goes dark
-        bright[i*DISPLAY_ROWS + j-1] = ((bright[i*DISPLAY_ROWS + j-1] - BRIGHTNESS_FADE) > MIN_LED_BRIGHTNESS) ? bright[i*DISPLAY_ROWS + j-1] - BRIGHTNESS_FADE : 0;
-        ledStrip.setPixelColor(i*DISPLAY_ROWS + j-1,PicoLed::HSV(color_hsv,255,bright[i*DISPLAY_ROWS + j-1]));
-        //This line is left here for reference
-        //leds[i*DISPLAY_ROWS + j-1] = PicoLed::HSV(color_hsv,255,bright[i*DISPLAY_ROWS + j-1]);
-      }
+        }
+        else {                                                 // Everything outside the range goes dark
+            bright[led_index] = ((bright[led_index] - BRIGHTNESS_FADE) > MIN_LED_BRIGHTNESS) ? bright[led_index] - BRIGHTNESS_FADE : 0;
+            ledStrip.setPixelColor(led_index,PicoLed::HSV(color_hsv,255,bright[led_index]));
+        }
     }
     color_hsv += 255 / DISPLAY_COL;                                    // Increment the Hue to get the Rainbow
+    //color_hsv = changeColor(color_hsv);
   }
 }
 
@@ -145,17 +155,17 @@ int main() {
     kiss_fft_scalar fft_in[CAPTURE_DEPTH]; // kiss_fft_scalar is a float
     kiss_fft_cpx fft_out[CAPTURE_DEPTH];
     float power[CAPTURE_DEPTH/2];
-    float outputPower[SPEED_BIN];
-    uint8_t intensityLED[SPEED_BIN];
+    float outputPower[FREQ_BIN];
+    uint8_t intensityLED[FREQ_BIN];
     float a0 = 25.0/46.0;
-
+    uint16_t max_adc_val_all_time = 0;
     kiss_fftr_cfg confg = kiss_fftr_alloc(CAPTURE_DEPTH,false,0,0);
 
     uint64_t startTime = time_us_64();
 
     
     ledStrip.setBrightness(64);
-    ledStrip.setPixelColor(20,PicoLed::RGB(255,255,255));
+    ledStrip.setPixelColor(LED_LENGTH-1,PicoLed::RGB(255,255,255));
     ledStrip.show();
 
     setup();
@@ -171,13 +181,20 @@ int main() {
         sample(capture_buf);
 
         // Compute DC offset value (should be 3.3V/s)
+        float avg = 3.3/2;
         uint64_t sum = 0;
-        for (int i=0;i<CAPTURE_DEPTH;i++) {sum+=capture_buf[i];}
-        float avg = (float)sum/CAPTURE_DEPTH * ADC_CONVERT;
+        uint16_t max_adc_val = 0;
+        // calculate average of the entire sample
+        // for (int i=0;i<CAPTURE_DEPTH;i++) {sum+=capture_buf[i];}
+        // float avg = (float)sum/CAPTURE_DEPTH * ADC_CONVERT;
 
         // Fill FFT input subtracting DC offset
         // Consider applying the hamming window to the sample
-        for (int i=0;i<CAPTURE_DEPTH;i++) {fft_in[i]=((float)capture_buf[i]*ADC_CONVERT-avg)*hammingWindow[i];}
+        for (int i=0;i<CAPTURE_DEPTH;i++) {
+            max_adc_val =  (capture_buf[i] > max_adc_val) ? capture_buf[i] : max_adc_val;
+            max_adc_val_all_time = (capture_buf[i] > max_adc_val_all_time) ? capture_buf[i] : max_adc_val_all_time;
+            fft_in[i]=((float)capture_buf[i] * ADC_CONVERT - avg) * hammingWindow[i];
+        }
 
         //computer fast fourier transform
         kiss_fftr(confg , fft_in, fft_out);
@@ -187,7 +204,8 @@ int main() {
         int max_idx = 0;
         int j = 0;
         float current_freq = 0;
-        outputPower[0] = 0;
+        uint8_t total_intensity = 0; //currently not used
+        memset(outputPower, 0, sizeof(outputPower)); 
         uint16_t current_freq_bin = 32;
         // any frequency bin over CAPTURE_DEPTH/2 is aliased (nyquist sampling theorum)
         for (int i = 0; i < CAPTURE_DEPTH/2; i++) {
@@ -200,23 +218,34 @@ int main() {
                 max_idx = i;
             }
 
-            if(j == SPEED_BIN)
+            //split the fft into frequency bins and get the max values for each bin. With the max value, then compute the LED intensity
+            if(j == FREQ_BIN)
             {
                 outputPower[j-1] = (power[i] > outputPower[j-1]) ? power[i] : outputPower[j-1];
                 if (i == CAPTURE_DEPTH/2 -1)
                 {
-                    intensityLED[j-1] = (outputPower[j-1] / magnitude_range * 15 > LED_ROWS) ? 15 : outputPower[j-1] / magnitude_range * 15;
+                    if (outputPower[j-1] < NOISE_LEVEL && j>4)
+                        intensityLED[j-1] = 0;
+                    else{
+                        intensityLED[j-1] = ((outputPower[j-1] - NOISE_LEVEL) / (magnitude_range - NOISE_LEVEL) * 15 > LED_ROWS) ? 15 : (outputPower[j-1] - NOISE_LEVEL) / (magnitude_range - NOISE_LEVEL) * 15;
+                        total_intensity += intensityLED[j-1];
+                    }
                 }
             }
             if (current_freq > current_freq_bin) 
             {
-                intensityLED[j] = (outputPower[j] / magnitude_range * 15 > LED_ROWS) ? 15 : outputPower[j] / magnitude_range * 15;
+                if (outputPower[j-1] < NOISE_LEVEL && j>4)
+                    intensityLED[j-1] = 0;
+                else{
+                    intensityLED[j-1] = ((outputPower[j] - NOISE_LEVEL) / (magnitude_range - NOISE_LEVEL) * 15 > LED_ROWS) ? 15 : (outputPower[j] - NOISE_LEVEL) / (magnitude_range - NOISE_LEVEL) * 15;
+                    total_intensity += intensityLED[j-1];
+                }
                 j++;
                 current_freq_bin = pow(2,j+5);
-                if(j < SPEED_BIN)
+                if(j < FREQ_BIN)
                     outputPower[j] = power[i];
             }
-            else if(j < SPEED_BIN)
+            else if(j < FREQ_BIN)
             {
                 outputPower[j] = (power[i] > outputPower[j]) ? power[i] : outputPower[j];
             }            
@@ -246,12 +275,15 @@ int main() {
 
         displayUpdate(intensityLED);
         ledStrip.show();
+        #ifdef STDIO
+            printf("%.5f %d %d\n",max_power, max_adc_val, max_adc_val_all_time);
+        #endif
 
         // DEBUG - output the outputPower array to read from USB
-        // for (int i = 0; i < SPEED_BIN; i++)
+        // for (int i = 0; i < FREQ_BIN; i++)
         // {
-        //     outputPower[i] = power[(int)(i * CAPTURE_DEPTH / SPEED_BIN)];
-        //     for (int j = (int)(i * CAPTURE_DEPTH / 2 / SPEED_BIN); j < (int)((i+1) * CAPTURE_DEPTH/ 2 / SPEED_BIN); j++)
+        //     outputPower[i] = power[(int)(i * CAPTURE_DEPTH / FREQ_BIN)];
+        //     for (int j = (int)(i * CAPTURE_DEPTH / 2 / FREQ_BIN); j < (int)((i+1) * CAPTURE_DEPTH/ 2 / FREQ_BIN); j++)
         //     {
         //         outputPower[i] = (outputPower[i] > power[j]) ? outputPower[i]:power[j];
         //     }
@@ -260,12 +292,12 @@ int main() {
 
         //Print samples to stdout so you can display them in pyplot, excel, matlab
         // for (int i = 0; i < CAPTURE_DEPTH/2; i++) {
-        //     printf("%.5f\n", (power[i]));   
+        //     printf("%.5f\n", (capture_buf[i]));   
         // }
         // printf("done\n");
         // sleep_ms(1000);
 
-        // for (int i = 0; i<SPEED_BIN;i++)
+        // for (int i = 0; i<FREQ_BIN;i++)
         // {
         //     printf("%.5f\n", (outputPower[i]));
         //     //printf("%d\n", (intensityLED[i]));
